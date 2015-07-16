88ee97f59e7c2fc770bbcad2c1294002cec27bf4
hmaruyama <maruyama@val.co.jp>
2015-07-16 12:11:59 +0900

    経路探索APIのサンプルを追加

--- sample3/js/sample.js
+++ sample3/js/sample.js
@@ -1,9 +1,9 @@
 $.ajax({
   type: 'GET',
-  url: 'http://api.ekispert.jp/v1/json/station',
+  url: 'http://api.ekispert.jp/v1/json/search/course/extreme',
   data: {
   key: '講義内でお伝えします',
-  name: '高円寺'
+  viaList: '高円寺:池袋'
   },
   dataType: 'json',
 })
@@ -12,14 +12,27 @@ $.ajax({
 
   // サイト上にレスポンスの中身を表示させる
   $(function(){
-    // 表示させたい要素
-    var name = data.ResultSet.Point[0].Station.Name;
-    var code = data.ResultSet.Point[0].Station.code;
-    var yomi = data.ResultSet.Point[0].Station.Yomi;
+    var stations = [];
+    var lines = [];
+    var line, station;
+    var route = data.ResultSet.Course[0].Route;
 
-    $("#show_name").text('駅名：' + name);
-    $("#show_code").text('コード：' + code);
-    $("#show_yomi").text('よみ：' + yomi);
+    stations[0] = route.Point[0].Station.Name;
+    station = $('<p>' + stations[0] + '</p>');
+    $("#course").append(station);
+
+    for(var i = 0; i < route.Point.length - 1; i++) {
+      stations[i + 1] = route.Point[i + 1].Station.Name;
+      if (route.Line instanceof Array) {
+        lines[i] = route.Line[i].Name;
+      } else {
+        lines[i] = route.Line.Name;
+      }
+      line = $('<p>| ' + lines[i] + '</p>');
+      station = $('<p>' + stations[i + 1] + '</p>');
+      $("#course").append(line);
+      $("#course").append(station);
+    }
   });
 
   // 開発ツールのコンソール上にレスポンス内容を表示する
